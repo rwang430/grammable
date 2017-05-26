22dedd6347a37ae0613fe3137c0e174b2c16594d
Roger Wang <blumoo@gmail.com>
2017-05-26 09:01:08 +0000

    gram validations test and pass

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe GramsController, type: :controller do
       expect(gram.message).to eq("Hello!")
     end
 
-    it "should properly dal with validation errors" do 
+    it "should properly deal with validation errors" do 
       post :create, params: { gram: { message: '' } }
       expect(response).to have_http_status(:unprocessable_entity)
       expect(Gram.count).to eq 0
