d314dd26973fc11c7a3acf388bb437265cbb3e7c
Roger Wang <blumoo@gmail.com>
2017-06-25 13:58:34 +0000

    edited comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "grams#index"
-  resources :grams
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :grams do 
+    resources :comments, only: :create
+  end
 end
